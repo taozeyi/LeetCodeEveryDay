a43fefced132b7ee72fd2f291a9e5375c86dc16e
zeyi tao <zeyi@TeTrahedron.local>
2015-08-27 17:22:15 -0400

    should read the problem carefully

--- longestSubstringWithoutRepeatingCharacters.py
+++ longestSubstringWithoutRepeatingCharacters.py
@@ -27,5 +27,38 @@ def lengthOfLongestSubstring(s):
 
 	return longestSubstring
 
-s = "cccccc"
+
+
+
+
+def otherlengthOfLongestSubstring(s):
+        start = 0
+        maxlen = 0
+        dict = {}
+        for i in range(len(s)):
+            dict[s[i]] = -1
+        for i in range(len(s)):
+            if dict[s[i]] != -1:
+                while start <= dict[s[i]]:
+                    dict[s[start]] = -1
+                    start += 1
+            if i - start + 1 > maxlen: maxlen = i - start + 1
+            dict[s[i]] = i
+        return maxlen
+
+
+s = "abcefghi"
 print lengthOfLongestSubstring(s)
+print otherlengthOfLongestSubstring(s)
+
+#########################################################################################
+# First Round:
+# i = 0, s[0] not in positions, 0 - (-1) > 0 longestSubstring = 0 - (-1) = 1, positions = {(a,0)}
+# Second Round:
+# i = 1, s[1] not in positions, 1 - (-1) > 1 longestSubstring = 1 - (-1) = 2, positions = {(a,0), (b,1)}
+# Thrid Round:
+# i = 2, s[2] not in positions, 2 - (-1) > 2 longestSubstring = 2 - (-1) = 3, positions = {(a,0), (b, 1), (c,2)}
+# Forth Round:
+# i = 3, s[3] = a in positions, and lastRepeating = -1 < positions[s[3]] = 0, lastRepeating = 0, positions{(a,3), (b, 1), (c,2)} 
+# Case 1 if "abcad"
+# i = 4, s[4] no in position, 4 - (-1)
